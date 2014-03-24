d0e65eb3adeabb3af17f20aca543811bc31cfbaf
jonathan7 <jjs1mail@gmail.com>
2014-03-23 22:26:42 -0700

    Ships move individually Menu improvements Preliminary hud component JLabelTest is now ShipSelectMenu Enterprise image fixed
    -removing ships from menu in wrong order still screws up layout
    
    Signed-off-by: jonathan7 <jjs1mail@gmail.com>

--- GameEngine.java
+++ GameEngine.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.awt.PointerInfo; //gets mouse position
 import java.awt.MouseInfo; //gets mouse position
 import java.awt.Toolkit; //gets screen resolution
+import javax.sound.sampled.*;
+import java.io.File;
 
 public class GameEngine
 {
@@ -24,31 +26,46 @@ public class GameEngine
         frame.setLocation(0,0);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        ArrayList<Double> startState = new ArrayList<Double>(); //starting state for one ship. Passed into contructor of ship class. 
-        startState.add(0.0); //x position
-        startState.add(0.0); //y position
-        startState.add(0.0); //x velocity (should be 0)
-        startState.add(0.0); //y velocity
-        startState.add(0.0); //x acceleration
-        startState.add(0.0); //y acceleration
-        startState.add(0.0); //'speed', current velocity overall (x and y)
-        startState.add(0.0); //current direction, in radians. Starts at positive x, clockwise
-        startState.add(0.0); //current turn speed, in radians per frame
-        ArrayList<Double> startState2 = new ArrayList<Double>(); //starting state for one ship. Passed into contructor of ship class. 
-        startState2.add(200.0); //x position
-        startState2.add(600.0); //y position
-        startState2.add(0.0); //x velocity (should be 0)
-        startState2.add(0.0); //y velocity
-        startState2.add(0.0); //x acceleration
-        startState2.add(0.0); //y acceleration
-        startState2.add(0.0); //'speed', current velocity overall (x and y)
-        startState2.add(3.0); //current direction, in radians. Starts at positive x, clockwise
-        startState2.add(0.0); //current turn speed, in radians per frame
+        //         ArrayList<Double> startState = new ArrayList<Double>(); //starting state for one ship. Passed into contructor of ship class. 
+        //         startState.add(0.0); //x position
+        //         startState.add(0.0); //y position
+        //         startState.add(0.0); //x velocity (should be 0)
+        //         startState.add(0.0); //y velocity
+        //         startState.add(0.0); //x acceleration
+        //         startState.add(0.0); //y acceleration
+        //         startState.add(0.0); //'speed', current velocity overall (x and y)
+        //         startState.add(0.0); //current direction, in radians. Starts at positive x, clockwise
+        //         startState.add(0.0); //current turn speed, in radians per frame
+        //         ArrayList<Double> startState2 = new ArrayList<Double>(); //starting state for one ship. Passed into contructor of ship class. 
+        //         startState2.add(200.0); //x position
+        //         startState2.add(600.0); //y position
+        //         startState2.add(0.0); //x velocity (should be 0)
+        //         startState2.add(0.0); //y velocity
+        //         startState2.add(0.0); //x acceleration
+        //         startState2.add(0.0); //y acceleration
+        //         startState2.add(0.0); //'speed', current velocity overall (x and y)
+        //         startState2.add(3.0); //current direction, in radians. Starts at positive x, clockwise
+        //         startState2.add(0.0); //current turn speed, in radians per frame
         final ArrayList<ShipComponent> shipList = ships;
+        final ArrayList<Integer> teamOneInd = new ArrayList<Integer>(); //tells which ships are on which teams
+        final ArrayList<Integer> teamTwoInd = new ArrayList<Integer>();
+        for(int i = 0; i < shipList.size(); i++)
+        {
+            if(shipList.get(i).getTeam()==1)
+            {
+                teamOneInd.add(i);
+            }
+            else 
+            {
+                teamTwoInd.add(i);
+            }
+        }
+
         //final ShipComponent a = new ShipComponent(startState, "Transporter", 1);
         //final ShipComponent b = new ShipComponent(startState2,"Cylon", 2);
         //shipList.add(a);
         //shipList.add(b);
+        final HudComponent h = new HudComponent();
         final ShipComponent[] currentlySelected = new ShipComponent[1];
         currentlySelected[0] = shipList.get(0);
         final PathComponent p = new PathComponent(400,400);
@@ -87,15 +104,16 @@ public class GameEngine
                         //it's also probably necessary for collision detection later
 
                         //System.out.println("\f" + state.toString());
+                        count++;
+                        if(count > 250)
+                        {
+                            pauseTurn = true;
+                            count = 0;
+                        }
                         for(ShipComponent s: shipList)
                         {
                             ArrayList<Double> state = s.getState(); //just used for debug, you can get all the values
-                            count++;
-                            if(count > 250)
-                            {
-                                pauseTurn = true;
-                                count = 0;
-                            }
+
                             if(s.readyToFire())
                             {
                                 allProjectiles.add(s.fire()); //add to array list
@@ -204,19 +222,65 @@ public class GameEngine
 
         class KeyTest implements KeyListener {
             private long lastPressProcessed = 0;
+            private int player;
+            private ArrayList<Integer> currentSelectable;
+            private ArrayList<Integer> p1Selectable;
+            private ArrayList<Integer> p2Selectable;
+            public KeyTest(ArrayList<Integer> p1i, ArrayList<Integer> p2i)
+            {
+                player = 1;
+                p1Selectable = p1i;
+                p2Selectable = p2i;
+                currentSelectable = p1i;
+            }
+
             public void keyTyped(KeyEvent e){
-                if(e.getKeyChar()=='p')
+                if(e.getKeyChar()=='b')
                 {
-                    pauseTurn = false;
-                    count = 0;
+                    
+                    System.out.println(player);
+                    if(player == 1)
+                    {
+                        playerSwitch();
+                    }
+                    else if (player==2)
+                    {
+                        playerSwitch();
+                        pauseTurn = false;
+                        count = 0;
+                    }
                 }
-                else if(e.getKeyChar()=='q')
+                else if(e.getKeyChar()=='1')
                 {
-                    currentlySelected[0] = shipList.get(0);
+                    currentlySelected[0] = shipList.get(currentSelectable.get(0));
                 }
-                else if(e.getKeyChar()=='w')
+                else if(e.getKeyChar()=='2' && currentSelectable.size()>1)
                 {
-                    currentlySelected[0] = shipList.get(1);
+                    currentlySelected[0] = shipList.get(currentSelectable.get(1));
+                }
+                else if(e.getKeyChar()=='3' && currentSelectable.size()>2)
+                {
+                    currentlySelected[0] = shipList.get(currentSelectable.get(2));
+                }
+                else if(e.getKeyChar()=='4' && currentSelectable.size()>3)
+                {
+                    currentlySelected[0] = shipList.get(currentSelectable.get(3));
+                }
+                else if(e.getKeyChar()=='5' && currentSelectable.size()>4)
+                {
+                    currentlySelected[0] = shipList.get(currentSelectable.get(4));
+                }
+                else if(e.getKeyChar()=='6' && currentSelectable.size()>5)
+                {
+                    currentlySelected[0] = shipList.get(currentSelectable.get(5));
+                }
+                else if(e.getKeyChar()=='7' && currentSelectable.size()>6)
+                {
+                    currentlySelected[0] = shipList.get(currentSelectable.get(6));
+                }
+                else if(e.getKeyChar()=='8' && currentSelectable.size()>7)
+                {
+                    currentlySelected[0] = shipList.get(currentSelectable.get(7));
                 }
             }
 
@@ -225,13 +289,29 @@ public class GameEngine
             }
 
             public void keyReleased(KeyEvent e){}
-        }
 
+            public void playerSwitch()
+            {
+                if(player == 1) 
+                {
+                    player = 2;
+
+                    currentSelectable = p2Selectable;
+                }
+                else if(player == 2)
+                {
+                    player = 1;
+                    currentSelectable = p1Selectable;
+                }
+
+            }
+        }
         t = new Timer(delay, frameTimer); 
         t.start();
         frame.addMouseListener(new MouseTest());
-        frame.addKeyListener(new 
-            KeyTest());
+        frame.addKeyListener(new KeyTest(teamOneInd, teamTwoInd));
+        frame.add(h);
+        frame.setVisible(true);
         for(ShipComponent s: shipList)
         {
             frame.add(s);
@@ -245,5 +325,25 @@ public class GameEngine
         frame.setVisible(true);
     }
 
-
+    public void playSound()
+    {
+        AudioInputStream music;
+        AudioInputStream music2;
+        try
+        {
+            music = AudioSystem.getAudioInputStream(new File("CrashSound.wav"));
+            music2 = AudioSystem.getAudioInputStream(new File("CrashSound2.wav"));
+            Clip clip2 = AudioSystem.getClip();
+            Clip clip = AudioSystem.getClip();
+            clip2.open(music2);
+            clip.open(music);
+            clip2.start();
+            clip.start();
+            //clip.loop(clip.LOOP_CONTINUOUSLY);
+        }
+        catch(Exception error)
+        {
+            // do nothing
+        }
+    }
 }
